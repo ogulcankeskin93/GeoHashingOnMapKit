9810d77c4f12505c4df4aaded60b46d8f7900020
oğulcan keskin <ogulcan.keskin@teb.com.tr>
2019-09-03 09:56:51 +0300

    Initial Commit

---
+++ GeoHashingOnMapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoHashingOnMapKit
+//
+//  Created by ogulcan keskin on 3.09.2019.
+//  Copyright © 2019 ogulcan keskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
